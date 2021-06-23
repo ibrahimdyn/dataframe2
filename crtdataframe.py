0e1272f039e03b6ed68fbc5d90772d4d6f3a4b71
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2021-06-23 23:41:11 +0200

    Create crtdataframe.py

---
+++ crtdataframe.py
@@ -0,0 +1,28 @@
+#import glob
+#import astropy.io.fits as fits
+#import numpy as np
+#import pandas as pd
+#from joblib import Parallel, delayed
+#import os
+import matplotlib.pyplot as plt
+import numpy as np
+import astropy.io.fits as fits
+import glob
+#from mpl_toolkits import mplot3d
+import astropy.wcs as wcs
+from astropy.table import Table
+import pandas as pd
+from astropy.coordinates import SkyCoord 
+from astropy.coordinates import Angle
+from astropy import units as u
+import matplotlib as mpl
+#import joblib
+from joblib import Parallel, delayed 
+
+imglst=glob.glob("/home/idayan/imglst/*.fits")
+
+len(imglst)
+
+print(len(imglst))
+
+imglst[0]
