346a4e67067207782d5f2a404e016d0316de1a3d
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2021-06-25 01:38:30 +0200

    Update testslurm.sh

--- testslurm.sh
+++ testslurm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#
+#SBATCH -N 1
 #SBATCH --ntasks=1
-#SBATCH --partition sixhour
+#SBATCH --cpus-per-task 12
 #SBATCH --time=6:00:00
 #SBATCH --array=1-10
 
 LINE=$(sed -n "$SLURM_ARRAY_TASK_ID"p File.txt)
 echo $LINE
 
-call-program-name-here $LINE
+#call-program-name-here $LINE
