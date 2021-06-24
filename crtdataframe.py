d30dee31e70db06681f47ef5367cb7db0a8ee4a8
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2021-06-24 04:37:35 +0200

    Update crtdataframe.py

--- crtdataframe.py
+++ crtdataframe.py
@@ -26,3 +26,31 @@ len(imglst)
 print(len(imglst))
 
 #imglst[0]
+
+imglst=glob.glob("/zfs/helios/filer0/mkuiack1/202008122000/*_all/SB*/imgs/*")
+
+N=len(imglst)
+#z=SkyCoord.from_name("PSR J1231-1411")
+#z=SkyCoord.from_name("4C 14.27") #
+def llc(filee):
+
+    hdulist= (fits.open(filee))[0]     
+    
+    w=wcs.WCS(hdulist.header) #
+    #wcoo=w.wcs_world2pix(231.794534, 51.100721 ,1,1,1) 
+    #wcoo=w.wcs_world2pix()
+    wcoo=w.wcs_world2pix(100, 100 ,1,1,1)  ##
+    arrwcoo=np.asarray(wcoo)  #
+   # aa=pd.DataFrame(columns =['I', 'Freq', 'Time'])
+   # aa=aa.append({'I':(hdulist.data)[0,0,int(arrwcoo[0]),int(arrwcoo[1])], 
+    
+   #               'Freq':hdulist.header["RESTFRQ"], 'Time':hdulist.header["DATE-OBS"][17:23]}, ignore_index=True)
+    
+    aa=pd.DataFrame(columns =['I', 'Freq', 'Time'])
+    aa=aa.append({'I':(hdulist.data)[0,0,int(arrwcoo[0]),int(arrwcoo[1])], 
+            'Freq':hdulist.header["CRVAL3"], 'Time':hdulist.header["DATE-OBS"][17:23]}, ignore_index=True)
+    return aa 
+
+%%timeit
+wrr=pd.DataFrame() 
+wrr=pd.concat(Parallel(n_jobs=-1, backend="multiprocessing", batch_size=2, verbose=10)(delayed(llc)(imglst[i]) for i in range(16000,16400)),ignore_index=True)
