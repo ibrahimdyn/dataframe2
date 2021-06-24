48a301071b02bc7498f55dd56cf16d7cf43d8f7b
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2021-06-24 05:20:24 +0200

    Update simpopr.sh

--- simpopr.sh
+++ simpopr.sh
@@ -2,4 +2,5 @@
 
 source /home/idayan/env/bin/activate 
 
-python /home/idayan/dataframe2/crtdataframe.py /home/idayan/imglst/*.fits
+#python /home/idayan/dataframe2/crtdataframe.py /home/idayan/imglst/*.fits
+python /home/idayan/dataframe2/crtdataframe.py /zfs/helios/filer0/mkuiack1/202008122000/*_all/SB*/imgs/*.fits
