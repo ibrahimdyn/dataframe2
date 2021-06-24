9c1ac384e86325ab7cf5b032cec32a652f149ba9
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2021-06-24 05:25:37 +0200

    Update simpopr.sh

--- simpopr.sh
+++ simpopr.sh
@@ -1,4 +1,10 @@
 #!/bin/bash 
+#SBATCH -N 1
+#SBATCH --ntasks-per-node 1
+#SBATCH --cpus-per-task 12
+#SBATCH --mem 20G
+#SBATCH --time 12:00:00
+#SBATCH --array=1-128%32
 
 source /home/idayan/env/bin/activate 
 
