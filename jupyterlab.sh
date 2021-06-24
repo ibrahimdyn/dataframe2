3bd2e7067cc0f7f77b40fe30b83e0a4d28e5c0bd
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2021-06-24 02:15:27 +0200

    Update jupyterlab.sh

--- jupyterlab.sh
+++ jupyterlab.sh
@@ -5,18 +5,9 @@
 #SBATCH --cpus-per-task 12
 #SBATCH --mem 20G
 #SBATCH --time 12:00:00
-#SBATCH --array=1-128%32
+
 #SBATCH --output=/home/idayan/slurm-%A_%a.out
-##SBATCH --output=/home/idayan/jupyter.log
 
-
-# #!/bin/bash
-# #SBATCH --job-name=jupyter
-# #SBATCH --partition=gpu
-# #SBATCH --gres=gpu:1
-# #SBATCH --time=2-00:00:00
-# #SBATCH --mem=50GB
-# #SBATCH --output=/home/<sunetID>/jupyter.log
 
 
 
