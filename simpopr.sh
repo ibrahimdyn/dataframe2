3d4020312420539fd405e5f6f6c0cd48cbf22223
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2021-06-28 00:34:44 +0200

    Update simpopr.sh

--- simpopr.sh
+++ simpopr.sh
@@ -7,6 +7,11 @@
 ##SBATCH --array=1-128%32
 
 source /home/idayan/env/bin/activate 
+
+slurm_job_id=os.environ['SLURM_ARRAY_TASK_ID']
+folders=list(glob.glob('/zfs/helios/filer0/mkuiack1/202008122000/*_all'))
+
+
 #cp /zfs/helios/filer0/mkuiack1/202008122000/*_all/SB*/imgs/*.fits /hddstore/idayan/
 #python /home/idayan/dataframe2/crtdataframe.py /home/idayan/imglst/*.fits
 #python /home/idayan/dataframe2/crtdataframe.py $SLURM_ARRAY_TASK_ID"/zfs/helios/filer0/mkuiack1/202008122000/*_all/SB*/imgs/*.fits"
