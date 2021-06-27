98106c404f6e8c8a148f1ce45b5f18c7aff111e0
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2021-06-28 00:44:29 +0200

    Update simpopr.sh

--- simpopr.sh
+++ simpopr.sh
@@ -1,15 +1,15 @@
 #!/bin/bash 
-##SBATCH -N 1
-##SBATCH --ntasks-per-node 1
-##SBATCH --cpus-per-task 12
-##SBATCH --mem 20G
-##SBATCH --time 12:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node 1
+#SBATCH --cpus-per-task 12
+#SBATCH --mem 20G
+#SBATCH --time 12:00:00
+#SBATCH --array=1-74%8
 ##SBATCH --array=1-128%32
 
 source /home/idayan/env/bin/activate 
 
-slurm_job_id=os.environ['SLURM_ARRAY_TASK_ID']
-folders=(glob.glob('/zfs/helios/filer0/mkuiack1/202008122000/*_all'))
+
 
 
 #cp /zfs/helios/filer0/mkuiack1/202008122000/*_all/SB*/imgs/*.fits /hddstore/idayan/
