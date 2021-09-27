7afdd9e23b3a6dde786ea93286927f1ec54f53ee
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2021-09-27 03:52:18 +0300

    Update PSRB09.sh

--- PSRB09.sh
+++ PSRB09.sh
@@ -17,4 +17,5 @@ source /home/idayan/env/bin/activate
 #python /home/idayan/dataframe2/crtdataframe.py $SLURM_ARRAY_TASK_ID"/zfs/helios/filer0/mkuiack1/202008122000/*_all/SB*/imgs/*.fits"
 
 
-python /home/idayan/dataframe2/crtdataframe.py 
+#python /home/idayan/dataframe2/crtdataframe.py 
+python /home/idayan/dataframe2/AllDatesDataFrame.py
