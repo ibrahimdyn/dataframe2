f8eb19967929c090601a4131fe5bce7eaacf253a
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2021-11-16 01:11:59 +0100

    Update PSRB09.sh

--- PSRB09.sh
+++ PSRB09.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks-per-node 1
 #SBATCH --cpus-per-task 8
 #SBATCH --mem 20G
-#SBATCH --time 12:00:00
+#SBATCH --time 24:00:00
 #SBATCH --output=/home/idayan/CALIBRATEDIMAGES/calibration.log
 
 
