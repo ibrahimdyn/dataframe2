784a4e054e316a5ba2b7c25b0da5a894fecf7e45
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2021-12-04 22:10:28 +0100

    Update PSRB09.sh

--- PSRB09.sh
+++ PSRB09.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks-per-node 1
 #SBATCH --cpus-per-task 10
 #SBATCH --mem 20G
-#SBATCH --time 12:00:00
+#SBATCH --time 42:00:00
 #SBATCH --output /home/idayan/CALwith60Mhz/calibration.log
 
 
