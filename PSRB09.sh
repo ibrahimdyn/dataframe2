ee43d7654ebc115e3b5896ec28705f5919010829
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2021-12-05 20:45:12 +0100

    Update PSRB09.sh

--- PSRB09.sh
+++ PSRB09.sh
@@ -4,7 +4,9 @@
 #SBATCH --cpus-per-task 10
 #SBATCH --mem 20G
 #SBATCH --time 42:00:00
-#SBATCH --output=/home/idayan/CALwith60Mhz/calibration.log
+#SBATCH --output=/zfs/helios/filer0/idayan/Cal60-20200812/calibration.log
+###  #### ((SBATCH --output=/home/idayan/CALwith60Mhz/calibration.log))
+
 
 
 
