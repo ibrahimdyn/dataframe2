5c90b2290e2a4eefdeb49dd0e7323e707674b3de
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2022-01-28 18:03:29 +0100

    Update calw2ref.sh

--- calw2ref.sh
+++ calw2ref.sh
@@ -37,3 +37,5 @@ source /home/idayan/new_env/bin/activate
 
 #python /home/idayan/dataframe2/GPHUNTgit.py
 python /home/idayan/dataframe2/GPHUNT202012032122.py
+python /home/idayan/dataframe2/calw2ref-202009240800.py
+
