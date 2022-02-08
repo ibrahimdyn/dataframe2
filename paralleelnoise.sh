1ae8416c16547147422108f84d8b9da80b46bc15
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2022-02-08 21:15:17 +0100

    Create paralleelnoise.sh

---
+++ paralleelnoise.sh
@@ -0,0 +1,64 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@ibrahimdyn 
+ibrahimdyn
+/
+dataframe2
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+dataframe2/noisegraph.sh
+@ibrahimdyn
+ibrahimdyn Update noisegraph.sh
+Latest commit 003ded6 9 hours ago
+ History
+ 1 contributor
+44 lines (24 sloc)  1.17 KB
+   
+#!/bin/bash 
+#SBATCH -N 1
+#SBATCH --ntasks-per-node 1
+#SBATCH --cpus-per-task 10
+#SBATCH --mem 20G
+#SBATCH --time 40:00:00
+#SBATCH --output=/home/idayan/noisegraphH.log
+###  #### SBATCH --output=/zfs/helios/filer0/idayan/Cal60-20200812/calibration.log
+###  #### ((SBATCH --output=/home/idayan/CALwith60Mhz/calibration.log))
+
+#source /home/idayan/env/bin/activate
+source /home/idayan/new_env/bin/activate
+
+
+
+
+#cp /zfs/helios/filer0/mkuiack1/202008122000/*_all/SB*/imgs/*.fits /hddstore/idayan/
+#python /home/idayan/dataframe2/crtdataframe.py /home/idayan/imglst/*.fits
+#python /home/idayan/dataframe2/crtdataframe.py $SLURM_ARRAY_TASK_ID"/zfs/helios/filer0/mkuiack1/202008122000/*_all/SB*/imgs/*.fits"
+
+
+#python /home/idayan/dataframe2/crtdataframe.py 
+#python /home/idayan/dataframe2/AllDatesDataFrame.py
+#python /home/idayan/dataframe2/AllDateDF-Automat.py
+#CALIBRATION202008122000.py
+#python /home/idayan/dataframe2/CALIBRATION202008122000.py
+#python /home/idayan/dataframe2/justcalib-only60MHz.py
+
+#python /home/idayan/dataframe2/noisegraph.py
+
+#python /home/idayan/dataframe2/GPHUNTgit.py
+#python /home/idayan/dataframe2/GPHUNT202012032122.py
+
+python /home/idayan/dataframe2/noisegraph.py
+
