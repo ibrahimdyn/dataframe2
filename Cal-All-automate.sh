a76462f36708cffcb2c9dd293fd6c52241103a2a
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2022-02-10 05:12:10 +0100

    Update Cal-All-automate.sh

--- Cal-All-automate.sh
+++ Cal-All-automate.sh
@@ -42,13 +42,13 @@ echo "your cwd is:" $(pwd)
 
 #c=`ls /zfs/helios/filer1/idayan/202006040630/*_all/SB*/imgs/*.fits`
 c=`ls /zfs/helios/filer1/idayan/$1/*_all/SB*/imgs/*.fits`
-ls $c > test44.txt
+ls $c > ~/test44.txt
 echo "echoing sed command; ready steady go:"
 #ls `sed $SLURM_ARRAY_TASK_ID'q;d' test44.txt`
 echo "echoED sed command!!!"
 #python /home/idayan/dataframe2/TESTcalautomate.py `sed $SLURM_ARRAY_TASK_ID'q;d' test44.txt` 
 #Cal-All-automate.py
-python /home/idayan/dataframe2/Cal-All-automate.py `sed $SLURM_ARRAY_TASK_ID'q;d' test44.txt` 
+python /home/idayan/dataframe2/Cal-All-automate.py `sed $SLURM_ARRAY_TASK_ID'q;d' ~/test44.txt` 
 
 #echo "this files will be processed:"
 #for i in "$b"; do echo "$i"; done
