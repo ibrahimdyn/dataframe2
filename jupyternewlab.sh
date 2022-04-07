83bcdfe8d2d205d18f03b644b00a137c36407784
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2022-04-08 01:26:39 +0200

    Update jupyternewlab.sh

--- jupyternewlab.sh
+++ jupyternewlab.sh
@@ -5,7 +5,7 @@
 #SBATCH --mem 20G
 #SBATCH --time 12:00:00
 #SBATCH -w helios-cn015
-#SBATCH --output=/home/idayan/jupyter.log
+#SBATCH --output=/home/idayan/jupyter15.log
 # #SBATCH --array=1-128%32
 
 
