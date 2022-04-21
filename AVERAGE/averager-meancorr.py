bb1411d4b95de8bc77e183bdc6b8e8bd9cd83a28
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2022-04-21 15:17:06 +0200

    Update averager-meancorr.py

--- AVERAGE/averager-meancorr.py
+++ AVERAGE/averager-meancorr.py
@@ -234,4 +234,4 @@ def img_averager(list_sametimestamps):
             fitsimg2.writeto('/zfs/helios/filer1/idayan/CALed/AVERAGED1/%s' % (DIR) +'/' + filename2, overwrite=True)
     return 
 if __name__= "__main__":
-  Parallel(n_jobs=11,backend="multiprocessing", verbose=10)(delayed(img_averager)(i) for i in matching)
+  Parallel(n_jobs=16,backend="multiprocessing", verbose=10)(delayed(img_averager)(i) for i in matching)
