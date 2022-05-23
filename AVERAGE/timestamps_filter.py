c7b6ec09646339f0df158fc4574ee0d94d4b50d1
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2022-05-23 14:51:48 +0200

    Update timestamps_filter.py

--- AVERAGE/timestamps_filter.py
+++ AVERAGE/timestamps_filter.py
@@ -79,7 +79,8 @@ from pandas import DataFrame
 
 
 
-with open('/home/idayan/rmngtoavrg202009290730.txt','r') as f: # wc -l rmngtoavrg202009290730.txt /home/idayan/tbavrgd202009290730.txt 102394
+with open('/home/idayan/toavrg202005121735.txt','r') as f: # wc -l  toavrg202005121735.txt 148906
+#with open('/home/idayan/rmngtoavrg202009290730.txt','r') as f: # wc -l rmngtoavrg202009290730.txt /home/idayan/tbavrgd202009290730.txt 102394
 #with open('/home/idayan/tbavrgd202009290730.txt','r') as f: # /home/idayan/tbavrgd202009290730.txt 218459
 #with open('/home/idayan/imgs202006051431toavrg.txt','r') as f: # two sec seperated imgs; check if they really like that; 87577
 #with open('/home/idayan/2-imgsin70-101102.txt','r') as f:
