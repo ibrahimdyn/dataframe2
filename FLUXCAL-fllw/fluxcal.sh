4b58c794d099b0b534011699c5cc587ec1246c75
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2022-05-24 20:05:40 +0200

    Update fluxcal.sh

--- FLUXCAL-fllw/fluxcal.sh
+++ FLUXCAL-fllw/fluxcal.sh
@@ -4,41 +4,12 @@
 #SBATCH --cpus-per-task 10
 #SBATCH --mem 40G
 #SBATCH --time 240:00:00
-#### ###SBATCH --array=13-17
 #SBATCH --array=1-255%50
-### #SBATCH --array=1-650%100
-### #SBATCH --exclude=helios-cn[016-020]
-#### #SBATCH --output=/home/idayan/TESTCALaut.log
-###  #### SBATCH --output=/zfs/helios/filer0/idayan/Cal60-20200812/calibration.log
-###  #### ((SBATCH --output=/home/idayan/CALwith60Mhz/calibration.log))
+
 
 
 #source /home/idayan/env/bin/activate
 source /home/idayan/new_env/bin/activate
-
-#echo your cwd is:
-#echo $(pwd)
-#for i in glob.glob("/zfs/helios/filer0/idayan/Calimgs/*.fits"):
-#b=`ls /zfs/helios/filer0/idayan/Calimgs/*.fits`
-#b=`ls /zfs/helios/filer1/idayan/$1/*_all/SB*/imgs/*.fits`
-
-#echo "this files will be processed:"
-#for i in "$b"; do echo "$i"; done
-#echo "processed files' end"
-
-#c=`ls /zfs/helios/filer1/idayan/202006040630/*_all/SB*/imgs/*.fits`
-#c=`ls /zfs/helios/filer1/idayan/$1/*_all/SB*/imgs/*.fits`
-#ls $c > ~/test44.txt
-#echo "echoing sed command; ready steady go"
-#ls `sed $SLURM_ARRAY_TASK_ID'q;d' test44.txt`
-#echo "echoED sed command!!!"
-#python /home/idayan/dataframe2/TESTcalautomate.py `sed $SLURM_ARRAY_TASK_ID'q;d' test44.txt` 
-#Cal-All-automate.py
-#python /home/idayan/dataframe2/Cal-All-automate.py `sed $SLURM_ARRAY_TASK_ID'q;d' ~/test44.txt` 
-
-#--fitsfile="$i"
-#python /home/idayan/dataframe2/Cal-All-automate.py  --fitsfile=`sed $SLURM_ARRAY_TASK_ID'q;d' ~/test44.txt`
-
 
 START=$SLURM_ARRAY_TASK_ID
 NUMLINES=1000
@@ -46,10 +17,8 @@ NUMLINES=1000
 STOP=$((SLURM_ARRAY_TASK_ID*NUMLINES))
 START="$(($STOP - $(($NUMLINES - 1))))"
 #START="$(($STOP - $(($NUMLINES - 1))))"
-
 echo "START=$START"
 echo "STOP=$STOP"
-
 for (( N = $START; N <= $STOP; N++ ))
 do
     echo $N
@@ -82,12 +51,4 @@ do
     #echo $(wc -l ~/ALL202007Dates2.txt)
     
 done
-
-
-
-#echo "this files will be processed:"
-#for i in "$b"; do echo "$i"; done
-#echo "processed files' end"
-#for i in $b;do python /home/idayan/dataframe2/Cal-All-automate.py --fitsfile=$i; done
-#python /home/idayan/dataframe2/Cal-All-automate.py --indir=/zfs/helios/filer0/idayan/Calimgs/ --fitsfile="$i"
 
