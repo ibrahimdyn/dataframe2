74db580074e07f00960a10b6d1d8724a8485120d
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2022-05-26 22:25:56 +0200

    Update parallelized-noisegraph.py

--- parallelized-noisegraph.py
+++ parallelized-noisegraph.py
@@ -122,8 +122,8 @@ def noisedist(img):
     #ranges=[0,100,200,300,400,500,600,700,800,900,1000]
     #ranges=[0,50,100,150,200,250,300,350,400,450,500,550,
     #        600,650,700,750,800,850,900,950,1000,1050,1097]
-    ranges=[0,50,100,150,200,250,300,350,400,450,500,550,
-            600,650,700,750,800,850,900,950,1000,1050,1097]
+    ranges=[0,25,50,75,100,125,150,175,200,225,250,275,300,325,350,375,400,425,450,475,500,525,550,
+            575,600,625,650,700,725,750,775,800,825,850,875,900,925,950,975,1000,1025,1050,1075,1100,1122]
     
     
         #hdl=fits.open(i)
@@ -135,7 +135,7 @@ def noisedist(img):
     resultlist = []
 
     for j in ranges:
-        annulus_aperture = CircularAnnulus(position, r_in=j, r_out=j+50)
+        annulus_aperture = CircularAnnulus(position, r_in=j, r_out=j+25)
         #skyregion = aperture.CircularAnnulus(position, r_in=j, r_out=j+100)
         #areaindeg = skyregion.area() * area1pix
     
@@ -220,7 +220,7 @@ if __name__ == "__main__":
     out=Parallel(n_jobs=10,backend="multiprocessing", verbose=10)(delayed(noisedist)(i) for i in fitslist)
     finalres=pd.DataFrame(out)
     #finalres.to_pickle("/home/idayan/noisegraphdfLAST000Co202005051300.pkl") 
-    finalres.to_pickle("/home/idayan/newnoisegrapDF/noisegraphdfAVRGF-{}.pkl".format(obs_folder))
+    finalres.to_pickle("/home/idayan/newnoisegrapDF/noisegraphdfAVRGF-NIT-{}.pkl".format(obs_folder))
     #finalres.to_pickle("/home/idayan/newnoisegrapDF/noisegraphdfLAST000Co-{}.pkl".format(obs_folder)) 
     #EDF.append(pd.DataFrame(out))
     #DF.append(pd.Series(out))
