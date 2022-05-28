613a7851d5e20ba5368e987ef24c2f25d43d7558
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2022-05-28 15:15:30 +0200

    Update paralleelnoise.sh

--- paralleelnoise.sh
+++ paralleelnoise.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node 1
 #SBATCH --cpus-per-task 10
 #SBATCH --mem 40G
-#SBATCH --time 05:00:00
+### ## ##SBATCH --time 05:00:00
 ## ####SBATCH --output=/home/idayan/noisedist.log
 ###  #### SBATCH --output=/zfs/helios/filer0/idayan/Cal60-20200812/calibration.log
 ###  #### ((SBATCH --output=/home/idayan/CALwith60Mhz/calibration.log))
