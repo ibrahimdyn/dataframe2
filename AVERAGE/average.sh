a2e81c9aad1148dfe61f4e4db02da51701066bd5
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2022-05-29 13:37:10 +0200

    Update average.sh

--- AVERAGE/average.sh
+++ AVERAGE/average.sh
@@ -4,11 +4,11 @@
 #SBATCH --ntasks-per-node 1
 #SBATCH --cpus-per-task 5
 #SBATCH --mem 60G
-#SBATCH --time 240:00:00
+### ## #SBATCH --time 240:00:00
 ##### ###SBATCH --array=13-17
 #### #SBATCH --array=1-650%200
 #### #SBATCH --exclude=helios-cn[016-020]
-#SBATCH --output=/home/idayan/LOGALL/AVERAGE-PROCESS.log
+### ###SBATCH --output=/home/idayan/LOGALL/AVERAGE-PROCESS.log
 ###  #### SBATCH --output=/zfs/helios/filer0/idayan/Cal60-20200812/calibration.log
 ###  #### ((SBATCH --output=/home/idayan/CALwith60Mhz/calibration.log))
 
