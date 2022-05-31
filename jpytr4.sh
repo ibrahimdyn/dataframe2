c5fb7e52b8c70e820af5afbd22998e99f0bf07b3
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2022-05-31 14:37:27 +0200

    Update jpytr4.sh

--- jpytr4.sh
+++ jpytr4.sh
@@ -5,7 +5,7 @@
 #SBATCH --mem 20G
 #SBATCH --time 5-00:00:00
 #SBATCH -w helios-cn004
-#SBATCH --output=/home/idayan/jupyter03.log
+#SBATCH --output=/home/idayan/jupyter4.log
 # #SBATCH --array=1-128%32
 
 
