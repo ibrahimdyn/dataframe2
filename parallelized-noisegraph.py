eb907dd5a0762c7ec7428f5e26e0c89b008fa5c2
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2022-06-01 00:10:42 +0200

    Update parallelized-noisegraph.py

--- parallelized-noisegraph.py
+++ parallelized-noisegraph.py
@@ -243,7 +243,7 @@ if __name__ == "__main__":
     out=Parallel(n_jobs=10,backend="multiprocessing", verbose=10)(delayed(noisedist)(i) for i in fitslist)
     finalres=pd.DataFrame(out)
     #finalres.to_pickle("/home/idayan/noisegraphdfLAST000Co202005051300.pkl") 
-    finalres.to_pickle("/home/idayan/newnoisegrapDF/noisegraphdfAVRGF-NITcr10inc-{}.pkl".format(obs_folder))
+    finalres.to_pickle("/home/idayan/newnoisegrapDF/AVRnoisegraphdfAVRGF-NITcr10inc-{}.pkl".format(obs_folder))
     #finalres.to_pickle("/home/idayan/newnoisegrapDF/noisegraphdfLAST000Co-{}.pkl".format(obs_folder)) 
     #EDF.append(pd.DataFrame(out))
     #DF.append(pd.Series(out))
