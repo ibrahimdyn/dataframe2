db6a1e015a1a6d1865f88bae815772066cb915e3
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2022-05-31 11:12:00 +0200

    Update Cal-All-automate.py

--- Cal-All-automate.py
+++ Cal-All-automate.py
@@ -205,7 +205,9 @@ def process(cfg):
 
         fields=[slope_cor,slope_err, intercept_cor,int_err, N_match]
 	#202009290730
-	with open(r'/home/idayan/fit2_202009290730_results.csv', 'a') as f:
+	#202005181400
+	with open(r'/home/idayan/fit2_202005181400_results.csv', 'a') as f:
+	#with open(r'/home/idayan/fit2_202009290730_results.csv', 'a') as f:
         #with open(r'/home/idayan/fit2_results.csv', 'a') as f:
             writer = csv.writer(f)
             writer.writerow(fields)
