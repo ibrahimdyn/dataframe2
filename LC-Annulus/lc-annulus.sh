d36a73ddeaf1e0d6086f5da104f43ae3bf3f57a3
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2022-06-17 02:03:40 +0200

    Update lc-annulus.sh

--- LC-Annulus/lc-annulus.sh
+++ LC-Annulus/lc-annulus.sh
@@ -5,7 +5,8 @@
 #SBATCH --mem 40G
 #SBATCH --time 240:00:00
 ##### ###SBATCH --array=13-17
-#SBATCH --array=1-520%100
+#SBATCH --array=1-20%10
+#### ### SBATCH --array=1-520%100
 ### ###SBATCH --exclude=helios-cn[016-020]
 #### #SBATCH --output=/home/idayan/TESTCALaut.log
 ###  #### SBATCH --output=/zfs/helios/filer0/idayan/Cal60-20200812/calibration.log
@@ -39,7 +40,8 @@ echo "echoED sed command!!!"
 
 
 START=$SLURM_ARRAY_TASK_ID
-NUMLINES=1000
+NUMLINES=10
+#NUMLINES=1000
 STOP=$((SLURM_ARRAY_TASK_ID*NUMLINES))
 START="$(($STOP - $(($NUMLINES - 1))))"
 #START="$(($STOP - $(($NUMLINES - 1))))"
