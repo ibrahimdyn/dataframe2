e5ae7e51504cbf56a16d8265dc07158d6ec0aaee
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2022-06-30 04:26:15 +0300

    Update BeamFluxCorr.py

--- BeamFluxCorr.py
+++ BeamFluxCorr.py
@@ -235,7 +235,7 @@ def process(cfg):
 	#202009290730
 	#202005181400
 	#ALLimgpathstofluxcal1-202005052000.txt
-  with open(r'/home/idayan/BeCa202009.csv', 'a') as f:
+        with open(r'/home/idayan/BeCa202009.csv', 'a') as f:
 	#with open(r'/home/idayan/beamcorcal202009.csv', 'a') as f:
 	#with open(r'/home/idayan/fit2_202005181400_results.csv', 'a') as f:
 	#with open(r'/home/idayan/fit2_202009290730_results.csv', 'a') as f:
