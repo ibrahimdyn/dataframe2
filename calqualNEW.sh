d265b0a390d8ff1dba788192f473df931246c919
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2022-08-26 16:03:00 +0200

    Update calqualNEW.sh

--- calqualNEW.sh
+++ calqualNEW.sh
@@ -93,7 +93,7 @@ do
     #echo $(wc -l ~/ALL3Dates.txt)
     #echo $(wc -l ~/REMAINIGDATES.txt)
     #echo $(wc -l ~/ALL202007Dates2.txt)
-    python /home/idayan/dataframe2/calqualNEW2.py $LINE
+    python /home/idayan/dataframe2/calqualNEW3.py $LINE
     #python /home/idayan/dataframe2/calqualNEW2.py $LINE
     #python /home/idayan/dataframe2/calqualNEW.py $LINE
     #python /home/idayan/dataframe2/CALQUAL5.py $LINE
