ef8f86cb57eb25d964636c2f6dbde2597a38e5ad
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2022-08-28 15:37:29 +0200

    Update calqualNEW3.py

--- calqualNEW3.py
+++ calqualNEW3.py
@@ -361,7 +361,7 @@ if __name__ == "__main__":
         
         ddf=pd.DataFrame([qualcalculator(IMG)])
         
-        ddf.to_csv("/home/idayan/myy0.csv", mode='a', index=False, header=False)
+        ddf.to_csv("/home/idayan/myy0WOB0604.csv", mode='a', index=False, header=False)
     except Exception as e:
         print "exception occured"
         
@@ -384,14 +384,14 @@ if __name__ == "__main__":
     #with open('/home/idayan/calqualNEWW0604.pkl', 'a+') as f2:
     print "first writing"
     #with open('/home/idayan/calqualNEWW0604woB.pkl', 'a+') as f2: # woB. # a+ not appending try ab
-    with open('/home/idayan/calqualNEWW0604woBab.pkl', 'ab') as f2: # woB # a+ not appending try ab
+    with open('/home/idayan/calqualNEWW0604woBabWOB.pkl', 'ab') as f2: # woB # a+ not appending try ab
     #with open('/home/idayan/GPsearch07DF1-5.pkl', 'wb') as ff:
         pickle.dump(BB, f2)
     print "first writing done"
     #with open(r'/home/idayan/calqualNEWcsvB.csv', 'a+') as f22:
     #with open(r'/home/idayan/calqualNEWcsvB0604.csv', 'a+') as f22:
     print "2 writing "
-    with open(r'/home/idayan/calqualNEWcsvB0604woBabBBB!!.csv', 'a+') as f22: #woB
+    with open(r'/home/idayan/calqualNEWcsvB0604woBabBBB!!WOB.csv', 'a+') as f22: #woB
         writer = csv.writer(f22)
         #writer.writerow([qualcalculator(IMG)])
         writer.writerow(qualcalculator(IMG))
