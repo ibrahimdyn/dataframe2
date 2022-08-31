4a1f9672ea3beb4d6e7ae10299039be811f476db
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2022-08-31 16:45:42 +0200

    Update timestampcreator.sh

--- AVERAGE/timestampcreator.sh
+++ AVERAGE/timestampcreator.sh
@@ -11,6 +11,6 @@
 ### ###SBATCH --output=/home/idayan/LOGALL/AVERAGE-PROCESS.log
 ###  #### SBATCH --output=/zfs/helios/filer0/idayan/Cal60-20200812/calibration.log
 ###  #### ((SBATCH --output=/home/idayan/CALwith60Mhz/calibration.log))
+FILE =$1
 
-
-python /home/idayan/dataframe2/AVERAGE/timestamps_filter_TEST.py
+python /home/idayan/dataframe2/AVERAGE/timestamps_filter_TEST.py FILE
