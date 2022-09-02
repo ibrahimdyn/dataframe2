07709f0612e2ab614046b4915bafc4cc09c856d4
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2022-09-02 14:37:40 +0200

    Update trap0512.sh

--- trap0512.sh
+++ trap0512.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node 1
 #SBATCH --cpus-per-task 6
 #SBATCH --mem 60G
-#SBATCH --time 240:00:00
+#SBATCH --time 480:00:00
 #SBATCH -w helios-cn003
 ##### ###SBATCH --array=13-17
 #### #SBATCH --array=1-650%200
@@ -27,9 +27,9 @@ echo "jobname is:"
 echo $1
 echo "starting processing"
 
-trap-manage.py run $1 -m "[[293.73,21.90], [293.73, 19.40],[293.73, 23.40],[291.43, 21.90]]"
+#trap-manage.py run $1 -m "[[293.73,21.90], [293.73, 19.40],[293.73, 23.40],[291.43, 21.90]]"
 
-#trap-manage.py run  $1  -m "[[148.56,7.66], [148.56, 10.16],[148.56, 5.16],[151.06, 7.66],[146.06, 7.66]]"
+trap-manage.py run  $1  -m "[[148.56,7.66], [148.56, 10.16],[148.56, 5.16],[151.06, 7.66],[146.06, 7.66]]"
 
 #trap-manage.py run  _2Averaged20K101102JOB  -m "[[148.56,7.66], [148.56, 10.16],[148.56, 5.16],[151.06, 7.66],[146.06, 7.66]]"
 
