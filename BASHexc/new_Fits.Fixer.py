145ca4b40c2d2eaee1c7817ce3094e02b89c2f6d
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2022-09-07 15:42:40 +0200

    Create new_Fits.Fixer.py

---
+++ BASHexc/new_Fits.Fixer.py
@@ -0,0 +1,18 @@
+#!/bin/python 
+
+
+from astropy.io import fits
+import glob
+import os
+import time 
+import sys
+from joblib import Parallel, delayed
+
+#images = glob.glob(sys.argv[1])
+images_folder = glob.glob(sys.argv[1]) # 202010030948
+images_sub_folder = sorted(glob.glob(images_folder)) # ... _all
+print images_sub_folder
+images_2sub_folder=  sorted(glob.glob(images_sub_folder))  # SB... SB.ms...
+print images_2sub_folder
+
+
