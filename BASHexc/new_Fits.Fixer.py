2f91637c2672863a0d05cc1cc50fe71015b09167
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2022-09-07 15:46:53 +0200

    Update new_Fits.Fixer.py

--- BASHexc/new_Fits.Fixer.py
+++ BASHexc/new_Fits.Fixer.py
@@ -12,7 +12,8 @@ print "AAAA"
 print sys.argv[1]
 print "BBB"
 #images = glob.glob(sys.argv[1])
-images_folder = glob.glob(sys.argv[1]) # 202010030948
+images_folder = glob.glob("/zfs/helios/filer1/idayan"+"".format(sys.argv[1])) # 202010030948
+print images_folder
 images_sub_folder = sorted(glob.glob(images_folder)) # ... _all
 print images_sub_folder
 images_2sub_folder=  sorted(glob.glob(images_sub_folder))  # SB... SB.ms...
