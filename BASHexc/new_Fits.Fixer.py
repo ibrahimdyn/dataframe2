3df707c17353aad33619b9adf02203cedeab626c
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2022-09-07 17:10:05 +0200

    Update new_Fits.Fixer.py

--- BASHexc/new_Fits.Fixer.py
+++ BASHexc/new_Fits.Fixer.py
@@ -11,12 +11,38 @@ from joblib import Parallel, delayed
 print "AAAA"
 print sys.argv[1]
 print "BBB"
+A=sys.argv[1]
 #images = glob.glob(sys.argv[1])
-images_folder = glob.glob("/zfs/helios/filer1/idayan"+"".format(sys.argv[1])) # 202010030948
-print images_folder
-images_sub_folder = sorted(glob.glob(images_folder)) # ... _all
-print images_sub_folder
-images_2sub_folder=  sorted(glob.glob(images_sub_folder))  # SB... SB.ms...
-print images_2sub_folder
 
+images=glob.glob("/zfs/helios/filer1/idayan/"+ str(A) + "/*_all" + "/SB*" + "/imgs/*.fits" )
 
+def fix_fits(img):
+    
+    hdulist = fits.open(img, mode='update')
+    prihdr = hdulist[0].header
+
+    # Don't know which of these is needed
+    prihdr.update({'RESTFRQ':hdulist[0].header['CRVAL3']})
+    prihdr.update({'RESTFREQ':hdulist[0].header['CRVAL3']})
+    
+    prihdr.update({'RESTBW':hdulist[0].header['CDELT3']})
+    
+    # These should be fit/written by WSClean
+    prihdr.update({'BMIN':0.2})
+    prihdr.update({'BMAJ':0.2})
+    
+
+    if hdulist[0].header['CRVAL1']<0.:
+        ra=hdulist[0].header['CRVAL1']+360.
+        prihdr['CRVAL1']=ra
+    
+    hdulist.flush()
+    
+    if os.path.basename(img)[:3] == "000":
+        newname = os.path.dirname(img)\
+        +"/"+str(hdulist[0].header["DATE-OBS"])\
+        +"-"+os.path.basename(img)[6:11]+".fits"
+
+        os.rename(img, newname)
+
+_out = Parallel(n_jobs=12)(delayed(fix_fits)(img) for img in images)
