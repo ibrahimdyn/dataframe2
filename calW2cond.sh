780e8fa8b34fa017e52a628bdd2a306e4896272f
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2022-09-27 15:42:11 +0200

    Update calW2cond.sh

--- calW2cond.sh
+++ calW2cond.sh
@@ -70,8 +70,10 @@ do
     #~/ALL-TXT/tocalqualNEW-202010060710.txt
     # -bash-4.2$ wc -l  ~/ALL-TXT/tocalqualNEW-202011080802.txt
     #56622 
-    LINE=$(sed -n "$N"p  /home/idayan/ALL-TXT/tocalqualNEW-202006041232.txt) # 56622
+     
+    LINE=$(sed -n "$N"p  /home/idayan/ALL-TXT/tocalqualNEW-202011100907.txt) # 56504
     
+    #LINE=$(sed -n "$N"p  /home/idayan/ALL-TXT/tocalqualNEW-202006041232.txt) # 56622
     #LINE=$(sed -n "$N"p  /home/idayan/ALL-TXT/fluxcal-remaining202011080802.txt) # 89496
     #LINE=$(sed -n "$N"p  /home/idayan/ALL-TXT/tocalqualNEW-202011080802.txt) # 169494
     #LINE=$(sed -n "$N"p  /home/idayan/ALL-TXT/tocalqualNEW-202006061232.txt) # 70769
