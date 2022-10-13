7648561183440d5b08cf5d4b7f9bf920897ddce7
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2022-10-13 18:34:38 +0200

    Update calW2cond.py

--- calW2cond.py
+++ calW2cond.py
@@ -336,7 +336,9 @@ def process(cfg):
 	#with open(r'/home/idayan/fit_results_202006041433.csv', 'a') as f:
 	
 	#with open(r'/home/idayan/fit_results_202010201235.csv', 'a') as f:
-	with open(r'/home/idayan/fit_results_202007231100.csv', 'a') as f:
+	
+	#with open(r'/home/idayan/fit_results_202007231100.csv', 'a') as f:
+	with open(r'/home/idayan/fit_results_202010201417.csv', 'a') as f:
 		
 	#tocalqualNEW-202010031118.txt
 	#tocalqualNEW-202010030948.txt
