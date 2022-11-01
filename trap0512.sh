3a4609cd3d980fec6c03074183ee905459649bdc
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2022-11-01 14:36:21 +0100

    Update trap0512.sh

--- trap0512.sh
+++ trap0512.sh
@@ -3,7 +3,7 @@
 #SBATCH -N -1
 #SBATCH --ntasks-per-node 1
 #SBATCH --cpus-per-task 6
-#SBATCH --mem 70G
+#SBATCH --mem 10G
 #SBATCH -w helios-cn009
 ##### ###SBATCH --array=13-17
 #### #SBATCH --array=1-650%200
