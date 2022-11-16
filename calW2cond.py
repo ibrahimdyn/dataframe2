019f32d4446be4ea06b9ded3da0a858d3856810a
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2022-11-16 17:32:27 +0100

    Update calW2cond.py

--- calW2cond.py
+++ calW2cond.py
@@ -54,7 +54,7 @@ def get_configuration():
     #parser.add_argument('--fitsfile', type=str, default="./",
     #                    help="Target fits file.")
 
-    parser.add_argument('--threshold', type=float, default = 1600.0,
+    parser.add_argument('--threshold', type=float, default = 2000.0,
                         help="RMS Threshold to reject image.")
     parser.add_argument('--outdir', type=str, default="/zfs/helios/filer1/idayan/UCALED/{}/".format(obs),
                         help="Desitnation directory.")
