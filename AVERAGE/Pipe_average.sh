4b1adbe26337d2ea84568194bce59a7a90c5134e
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2023-01-14 00:47:16 +0100

    Create Pipe_average.sh

---
+++ AVERAGE/Pipe_average.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+### ### #SBATCH --partition neutron-star
+#SBATCH -N 1
+#SBATCH --ntasks-per-node 1
+#SBATCH --cpus-per-task 7
+#SBATCH --mem 60G
+### ## #SBATCH --time 240:00:00
+##### ###SBATCH --array=13-17
+#### #SBATCH --array=1-650%200
+#### #SBATCH --exclude=helios-cn[016-020]
+### ###SBATCH --output=/home/idayan/LOGALL/AVERAGE-PROCESS.log
+###  #### SBATCH --output=/zfs/helios/filer0/idayan/Cal60-20200812/calibration.log
+###  #### ((SBATCH --output=/home/idayan/CALwith60Mhz/calibration.log))
+
+python ~/dataframe2/AVERAGE/timestamps_filter_pipe.py $1 $2
+
+#source /home/idayan/new_env/bin/activate
+#python /home/idayan/dataframe2/AVERAGE/average-frq-corr.py
