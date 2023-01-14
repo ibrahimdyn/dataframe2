6ab918828c4f074da7f513ba93a673180154e523
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2023-01-14 01:15:10 +0100

    Update pipe_head_tmstmps.sh

--- AVERAGE/PIPE_AVERAGE/pipe_head_tmstmps.sh
+++ AVERAGE/PIPE_AVERAGE/pipe_head_tmstmps.sh
@@ -12,6 +12,7 @@
 ###  #### SBATCH --output=/zfs/helios/filer0/idayan/Cal60-20200812/calibration.log
 ###  #### ((SBATCH --output=/home/idayan/CALwith60Mhz/calibration.log))
 
+source /home/idayan/new_env/bin/activate
 python ~/dataframe2/AVERAGE/PIPE_AVERAGE/Pipe_timestamps.py $1 $2
 
 #source /home/idayan/new_env/bin/activate
