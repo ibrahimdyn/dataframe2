e64650a5961172e4b4f56da851b3216f8da4cb34
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2023-02-21 14:55:36 +0100

    Update calW2cond.py

--- calW2cond.py
+++ calW2cond.py
@@ -376,7 +376,8 @@ def process(cfg):
 	
         #if slope_cor < 1e8:
 	# try with 0.45; for the date 202006041232 only 1 percent pass the 0.40 std filter
-        if (slope_cor < 1e8) and ( _STD_ < 0.45): # for the date 202006041232 only 1 percent pass the 0.40 std filter
+	if (slope_cor < 1e8) and ( _STD_ < 0.55):
+        #if (slope_cor < 1e8) and ( _STD_ < 0.45): # for the date 202006041232 only 1 percent pass the 0.40 std filter
 	        filename = '%s.fits' % (datetime.fromtimestamp(t.unix).strftime('%Y-%m-%dT%H:%M:%S')+ \
 		    "-S"+str(round((frq-lofarfrequencyOffset)/lofarBW,1))+ \
 		    "-B"+str(int(np.ceil(bw /lofarBW))))
